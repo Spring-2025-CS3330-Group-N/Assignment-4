b99186ad96261a3a2a19f7ee0913107a11fa6c8a
Alyssa Norrenberns <alyssa.norrenberns@gmail.com>
2025-04-21 23:03:37 -0500

    added javadocs for Row and Grid

--- src/hw4/maze/Grid.java
+++ src/hw4/maze/Grid.java
@@ -2,21 +2,42 @@ package hw4.maze;
 
 import java.util.ArrayList;
 
+/**
+ * This class is used to group rows.
+ * It consists of an ArrayList of Rows.
+ */
 public class Grid {
 	private ArrayList<Row> rows;
 
+	/**
+	 * Parameterized constructor for Grid():
+	 * Creates a grid using the provided list of rows.
+	 * @param rows List of rows
+	 */
 	public Grid(ArrayList<Row> rows) {
 		this.setRows(rows);
 	}
 
+	/**
+	 * This getter function returns a list of rows in the grid.
+	 * @return A list of rows
+	 */
 	public ArrayList<Row> getRows() {
 		return rows;
 	}
 
+	/**
+	 * This setter function assigns the grid a new list of rows.
+	 * @param rows A list of rows
+	 */
 	public void setRows(ArrayList<Row> rows) {
 		this.rows = rows;
 	}
 
+	/**
+	 * toString():
+	 * This method prints all the rows in a given grid.
+	 */
 	@Override
 	public String toString() {
 		return "Grid [rows=" + rows + "]";
--- src/hw4/maze/Row.java
+++ src/hw4/maze/Row.java
@@ -2,21 +2,42 @@ package hw4.maze;
 
 import java.util.ArrayList;
 
+/**
+ * This class is used to groups cells.
+ * It consists of an ArrayList of Cells.
+ */
 public class Row {
 	private ArrayList<Cell> cells;
 
+	/**
+	 * Parameterized constructor for Row():
+	 * Creates a row using the provided list of cells.
+	 * @param cells List of cells to be added
+	 */
 	public Row(ArrayList<Cell> cells) {
 		this.setCells(cells);
 	}
 
+	/**
+	 * This getter function returns a list of cells in the row.
+	 * @return All the cells in the given row.
+	 */
 	public ArrayList<Cell> getCells() {
 		return cells;
 	}
 
+	/**
+	 * This setter function assigns the row a new list of cells.
+	 * @param cells List of cells
+	 */
 	public void setCells(ArrayList<Cell> cells) {
 		this.cells = cells;
 	}
 
+	/**
+	 * toString():
+	 * This method prints all the cells in a given row.
+	 */
 	@Override
 	public String toString() {
 		return "Row [cells=" + cells + "]";
