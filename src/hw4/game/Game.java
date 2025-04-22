9083aef9217678c60328aae6285114973e43de75
Alyssa Norrenberns <alyssa.norrenberns@gmail.com>
2025-04-21 23:34:31 -0500

    added javadocs for Game and Player

--- src/hw4/game/Game.java
+++ src/hw4/game/Game.java
@@ -9,30 +9,66 @@ import hw4.maze.Row;
 import hw4.player.Movement;
 import hw4.player.Player;
 
+/**
+ * This class is used to provide game functionality for the maze.
+ * It contains a grid of rows and cells.
+ */
 public class Game {
 	private Grid grid;
 
+	/**
+	 * Parameterized constructor for Grid():
+	 * Creates a game using the provided argument.
+	 * @param grid Grid to be used for the maze game
+	 */
 	public Game(Grid grid) {
 		this.grid = grid;
 	}
 
+	/**
+	 * This alternative constructor creates a random grid.
+	 * @param n Size of the grid
+	 */
 	public Game(int n) {
 		this.grid = createRandomGrid(n);
 	}
 
+	/**
+	 * getGrid():
+	 * This getter method retrieves a grid from the Game object it's called upon.
+	 * @return The grid assigned to the game
+	 */
 	public Grid getGrid() {
 		return grid;
 	}
 
+	/**
+	 * This setter method assigns a grid to the Game object it's called upon.
+	 * @param grid Grid to be used for the game
+	 */
 	public void setGrid(Grid grid) {
 		this.grid = grid;
 	}
 
+	/**
+	 * play():
+	 * This method updates a character's position based on the given input variables
+	 * @param right The direction a player will move (ex. left, right, up, down)
+	 * @param player The player to be moved
+	 * @return True if the movement and player are valid, return False if otherwise.
+	 */
 	public boolean play(Movement right, Player player) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * createRandomGrid():
+	 * This method creates a random grid based on the size argument passed in.
+	 * It checks to ensure the size argument is greater than or equal to three as well as less than or equal to 8.
+	 * @param n Desired dimension (size) for the grid
+	 * @return A new random grid of size n upon success, otherwise return null.
+	 */
 	public Grid createRandomGrid(int n) {
 		if (2 >= n || n >= 8) {
 			return null;
@@ -43,6 +79,10 @@ public class Game {
 		return grid;
 	}
 
+	/**
+	 * toString():
+	 * This method prints the grid details used for the given game.
+	 */
 	@Override
 	public String toString() {
 		return "Game [grid=" + grid + "]";
--- src/hw4/player/Player.java
+++ src/hw4/player/Player.java
@@ -3,31 +3,61 @@ package hw4.player;
 import hw4.maze.Cell;
 import hw4.maze.Row;
 
+/**
+ * This class includes information on where the player is located in the maze.
+ * It contains the player's current row as well as their current cell.
+ */
 public class Player {
 	private Row currentRow;
 	private Cell currentCell;
 
+	/**
+	 * Parameterized constructor for Player():
+	 * Creates a player at a given location in the maze based on the provided arguments.
+	 * @param currentRow The desired row position of the player
+	 * @param currentCell The desired cell position of the player
+	 */
 	public Player(Row currentRow, Cell currentCell) {
 		this.currentRow = currentRow;
 		this.currentCell = currentCell;
 	}
 
+	/**
+	 * This getter method returns the current row position of the player.
+	 * @return the player's row position
+	 */
 	public Row getCurrentRow() {
 		return currentRow;
 	}
 
+	/**
+	 * This setter method assigns the player's current row to the provided method argument.
+	 * @param currentRow Desired row position for the player
+	 */
 	public void setCurrentRow(Row currentRow) {
 		this.currentRow = currentRow;
 	}
 
+	/**
+	 * This getter method returns the current cell position of the player.
+	 * @return The player's cell position
+	 */
 	public Cell getCurrentCell() {
 		return currentCell;
 	}
 
+	/**
+	 * This setter method assigns the player's current cell to the provided method argument.
+	 * @param currentCell Desired cell position for the player
+	 */
 	public void setCurrentCell(Cell currentCell) {
 		this.currentCell = currentCell;
 	}
 
+	/**
+	 * toString():
+	 * This method prints the player's location in the maze using their current cell and row positions.
+	 */
 	@Override
 	public String toString() {
 		return "Player [currentCell=" + currentCell + ", currentRow=" + currentRow + "]";
