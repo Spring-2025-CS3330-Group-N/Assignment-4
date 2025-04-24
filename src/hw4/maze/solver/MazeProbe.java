a4ab28d4a95381110612f47b5baef96d9cd16f74
creallfluharty <creallfluharty@gmail.com>
2025-04-23 22:18:14 -0500

    maze solver + sketcher docstrings

--- src/hw4/maze/solver/MazeProbe.java
+++ src/hw4/maze/solver/MazeProbe.java
@@ -1,6 +1,21 @@
 package hw4.maze.solver;
 
 public interface MazeProbe {
+	/**
+	 * determine whether a player at (x, y) has escaped the maze
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	boolean checkEscaped(int x, int y);
+
+	/**
+	 * attempt to move the player from (x1, y1) to (x2, y2) and return true/false depending on whether the move was successful
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 * @return
+	 */
 	boolean attemptMove(int x1, int y1, int x2, int y2);
 }
--- src/hw4/maze/solver/MazeSolver.java
+++ src/hw4/maze/solver/MazeSolver.java
@@ -14,45 +14,58 @@ public class MazeSolver {
 	private int directionIndex;
 	private Movement direction;
 
+	/**
+	 * constructs a maze solver starting at (x, y)
+	 * @param x
+	 * @param y
+	 */
 	public MazeSolver(int x, int y) {
 		this.coordinate = new Coordinate(x, y);
 		setDirection(0); // any direction is fine to start
 	}
 
+	/**
+	 * private helper function for setting the direction to keep the rotation system in sync
+	 * @param directionIndex
+	 */
 	private void setDirection(int directionIndex) {
 		this.directionIndex = directionIndex;
 		this.direction = this.directions[directionIndex];
 	}
 
 	/**
-	 * since java doesn't have this built in :|
+	 * actual modulo since java's % doesn't work for negative numbers
 	 * @param a
 	 * @param b
 	 * @return
 	 */
 	private int modulo(int a, int b) {
-		return (a % b + b) % b;
+		return (a % b + b) % b; // a % b gets us within (-b, b), + b within the positive range, and % b for the actual result
 	}
 
 	/**
 	 * rotate the direction 90*dir degrees counter-clockwise
-	 * @param dir
+	 * @param dir (ex: +1 means +90 deg, -1 mean -90 deg)
 	 */
 	private void rotate(int dir) {
 		var directionIndex = this.modulo(this.directionIndex + dir, this.directions.length);
 		setDirection(directionIndex);
 	}
 
+	/**
+	 * solve the maze using a "follow the right wall" strategy
+	 * @param probe
+	 */
 	public void solve(MazeProbe probe) {
 		while (!probe.checkEscaped(this.coordinate.getX(), this.coordinate.getY())) {
-			this.rotate(-1);
+			this.rotate(-1); // this more or less works to find the right wall
 			while (true) {
 				Coordinate next = this.coordinate.add(this.direction);
 				if (probe.attemptMove(coordinate.getX(), coordinate.getY(), next.getX(), next.getY())) {
 					this.coordinate = next;
 					break;
 				}
-				this.rotate(1);
+				this.rotate(1); // while this turns until you can move forward after finding the wall
 			}
 		}
 	}
